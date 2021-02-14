444ab5d672d19fe73a2827bc03139ea5e8038e28
atrem13 <mertayoga17.my@gmail.com>
2021-02-14 22:09:38 +0800

    pet controller done

--- api/controllers/PetController.js
+++ api/controllers/PetController.js
@@ -6,7 +6,75 @@
  */
 
 module.exports = {
-  
+
+  async create(req, res){
+    try {
+      let params = req.allParams();
+      let results = await Pet.create({
+        name: params.name,
+        race: params.race,
+      }).fetch();
+      return res.json(results);
+    }catch(err){
+      return res.serverError(err);
+    }
+  },
+
+  async findOne(req, res){
+    try {
+      let pet = await Pet.findOne({
+        id: params.id,
+      });
+      return res.json(pet);
+    }catch(err){
+      return res.serverError(err);
+    }
+  },
+
+  async find(req, res){
+    try {
+      let pets = await Pet.find();
+      return res.json(pets);
+    }catch(err){
+      return res.serverError(err);
+    }
+  },
+
+  async update(req, res){
+    try {
+      let params = req.allParams();
+      let attributes = {};
+      if(params.name){
+        attributes.name = params.name;
+      }
+      if(params.race){
+        attributes.race = params.race;
+      }
+      let result = await Pet.update(
+        {
+          id: params.id,
+        },
+        attributes,
+        {updatedAt: Date.now()}
+      );
+      return res.json(result);
+    }catch(err){
+      return res.serverError(err);
+    }
+  },
+
+  async create(req, res){
+    try {
+      let result = await Pet.destroy(
+        {
+          id: req.params.id
+        }
+      );
+      return res.json(result);
+    }catch(err){
+      return res.serverError(err);
+    }
+  },
 
 };
 
